4d105c8bfff54902486270ed58bb006cd372df44
Ing <wjz304@qq.com>
2023-10-22 09:58:23 +0000

    RR init

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -34,40 +34,40 @@ fdisk -l grub.img
 
 LOOPX=$(sudo losetup -f)
 sudo losetup -P ${LOOPX} grub.img
-sudo mkdosfs -F32 -n ARPL1 ${LOOPX}p1
-sudo mkfs.ext2 -F -L ARPL2 ${LOOPX}p2
-sudo mkfs.ext4 -F -L ARPL3 ${LOOPX}p3
+sudo mkdosfs -F32 -n RR1 ${LOOPX}p1
+sudo mkfs.ext2 -F -L RR2 ${LOOPX}p2
+sudo mkfs.ext4 -F -L RR3 ${LOOPX}p3
 
-rm -rf ARPL1
-mkdir -p ARPL1
-sudo mount ${LOOPX}p1 ARPL1
+rm -rf RR1
+mkdir -p RR1
+sudo mount ${LOOPX}p1 RR1
 
-sudo mkdir -p ARPL1/EFI
-sudo mkdir -p ARPL1/boot/grub
+sudo mkdir -p RR1/EFI
+sudo mkdir -p RR1/boot/grub
 cat >device.map <<EOF
 (hd0)   ${LOOPX}
 EOF
-sudo mv device.map ARPL1/boot/grub/device.map
+sudo mv device.map RR1/boot/grub/device.map
 
 for B in ${BIOS}; do
   args=""
-  args+=" ${LOOPX} --target=${B} --no-floppy --recheck --grub-mkdevicemap=ARPL1/boot/grub/device.map --boot-directory=ARPL1/boot"
+  args+=" ${LOOPX} --target=${B} --no-floppy --recheck --grub-mkdevicemap=RR1/boot/grub/device.map --boot-directory=RR1/boot"
   if [[ "${B}" == *"efi" ]]; then
-    args+=" --efi-directory=ARPL1 --removable --no-nvram"
+    args+=" --efi-directory=RR1 --removable --no-nvram"
   else
-    args+=" --root-directory=ARPL1"
+    args+=" --root-directory=RR1"
   fi
   sudo ${GRUB}/${B}/grub-install ${args}
 done
 
-if [ -d ARPL1/boot/grub/fonts -a -f /usr/share/grub/unicode.pf2 ]; then
-  sudo cp /usr/share/grub/unicode.pf2 ARPL1/boot/grub/fonts
+if [ -d RR1/boot/grub/fonts -a -f /usr/share/grub/unicode.pf2 ]; then
+  sudo cp /usr/share/grub/unicode.pf2 RR1/boot/grub/fonts
 fi
 
 sudo sync
 
 sudo umount ${LOOPX}p1
 sudo losetup -d ${LOOPX}
-sudo rm -rf ARPL1
+sudo rm -rf RR1
 
 gzip grub.img
